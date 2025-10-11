- decoupled clipped value - Add Reinforce Trainer - Fix bug (Softmax by batch dim in reinforce loss) - Add simple constructor for build model - Add InvertedPendulum env config

common/utils.py
@@ -7,6 +7,7 @@ import os
 import signal
 import json
 import time
+import torch.nn as nn
 
 
 def stack_dict(data: dict, axis=0):
@@ -35,7 +36,6 @@ def reinforce_loss(pred: torch.Tensor, rewards: torch.Tensor):
 
 
 def is_port_running(port):
-    port = 8501
     url = f"http://127.0.0.1:{port}"
     try:
         r = requests.get(url)
@@ -49,17 +49,17 @@ def is_port_running(port):
 def kill_process_on_port(port: int):
     try:
         result = subprocess.check_output(
-            ["lsof", "-nP", "-i", f"TCP:{port}"]
+            ["lsof", "-nP", "-i", f"TCP:{port}", "+c", "15"]
         ).decode()
         for line in result.splitlines()[1:]:
             cols = line.split()
             pid = int(cols[1])
             cmd = cols[0]
-            if "tensorboard" in cmd.lower() or "python" in cmd.lower():
+            if "tensorboard" in cmd.lower() or "python" in cmd.lower() or "streamlit" in cmd.lower():
                 print(f"Killing {cmd} (PID {pid})...")
                 os.kill(pid, signal.SIGKILL)
             else:
-                print(f"Skipping {cmd} — not TensorBoard.")
+                print(f"Skipping {cmd}")
     except subprocess.CalledProcessError:
         print(f"No process found on port {port}.")
 
@@ -90,16 +90,29 @@ def restart_tensorboard(port):
             tmp.append(f"{run['alias']}:{run['run_id']}/")
     run_with_alias = ",".join(tmp)
 
-    if is_port_running(port):
-        kill_process_on_port(port)
+    kill_process_on_port(port)
     try:
         # Запуск TensorBoard в subprocess
         subprocess.Popen(
             ["tensorboard", "--logdir_spec", run_with_alias, "--port", str(port)],
-            # "--logdir_spec exp1:/path/to/run1,exp2:/path/to/run2"
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
         return f"TensorBoard запущен на порту {port}", False
     except Exception as e:
         return f"Ошибка запуска TensorBoard: {e}", True
+
+
+def build_mlp(input_dim, hidden_dim, depth, output_dim=None, activation="ReLU", normalization="LayerNorm"):
+    layers = []
+    act = getattr(nn, activation)
+    norm = getattr(nn, normalization)
+    prev_dim = input_dim
+    for _ in range(depth):
+        layers.append(nn.Linear(prev_dim, hidden_dim))
+        layers.append(act())
+        layers.append(norm(hidden_dim))
+        prev_dim = hidden_dim
+    if output_dim is not None:
+        layers.append(nn.Linear(prev_dim, output_dim))
+    return nn.Sequential(*layers)

core/checkpoint.py
@@ -1,7 +1,7 @@
 import os
 import torch
 import shutil
-from model.base import ModelBase
+from model.base import AutoModel
 from gymnasium import Env
 from logger.base import LoggerBase
 from pathlib import Path
@@ -13,7 +13,7 @@ from tensorboard.backend.event_processing import event_accumulator
 class CheckPointer:
     def __init__(
         self,
-        model: ModelBase,
+        model: AutoModel,
         optimizer: torch.optim.Optimizer,
         env: Env,
         logger: LoggerBase,

core/dataminer.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 from torch.utils.data import DataLoader
-from model.base import ModelBase
+from model.base import AutoModel
 from gymnasium import Env
 from common.utils import stack_dict, collate_to_device, list_dict_extend, reinforce_loss
 
@@ -9,7 +9,7 @@ from common.utils import stack_dict, collate_to_device, list_dict_extend, reinfo
 class SimpleDataMiner:
     def __init__(
             self, 
-            model: ModelBase, 
+            model: AutoModel, 
             env: Env,
             batch_size,
             eval_seeds,

core/trainer.py
@@ -1,44 +1,76 @@
 import torch
 from common.utils import reinforce_loss
-from model.base import ModelBase
+from model.base import AutoModel
 
 
 class BaseTrainer:
-    def step(self, *args, **kwargs):
-        raise NotImplementedError
+    def __init__(
+            self,
+            *,
+            model: AutoModel,
+            optimizer: torch.optim.Optimizer=None,
+            entropy_coef=0.01,
+        ):
+        self.model = model
+        self.optimizer = optimizer
+        self.entropy_coef = entropy_coef
+
+    def step(self, **kwargs):
+        loss, metrics = self.loss(**kwargs)
+
+        self.optimizer.zero_grad()
+
+        loss.backward()
+
+        metrics.update(self.model.clip())
+
+        self.optimizer.step()
+
+        return metrics
 
     def loss(self, *args, **kwargs):
         raise NotImplementedError
 
 
-class ValueReinforceTrainer(BaseTrainer):
-    """Reinforce with Value function (using GAE)"""
+class ReinforceTrainer(BaseTrainer):
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+    def loss(self, state, action, advantage, *args, **kwargs):
+        pred, entropy_loss, _ = self.model.step(state, action)
+
+        rl_loss = -(pred * advantage).mean()
+
+        loss = rl_loss + entropy_loss * self.entropy_coef
+
+        return loss, {
+            "loss": loss.item(),
+            "rl_loss": rl_loss.item(),
+            "entropy_loss": entropy_loss.item(),
+        }
+
+
+class A2CTrainer(BaseTrainer):
+    """Reinforce with Value function"""
     def __init__(
             self,
-            model: ModelBase,
-            optimizer: torch.optim.Optimizer=None,
-            entropy=False,
-            entropy_coef=0.01,
-            value_coef=1,
-            clip_value = 5,
+            *,
+            value_coef,
+            **kwargs,
         ):
-        self.model = model
-        self.optimizer = optimizer
-        self.entropy = entropy
-        if entropy:
-            self.entropy_coef = entropy_coef
-        else:
-            self.entropy_coef = 0
-        self.clip_value = clip_value
+        super().__init__(**kwargs)
         self.value_coef = value_coef
 
-    def loss(self, state, action, reward, advantage, value_target):
-        pred, entropy_loss, value = self.model.step(state, action, self.entropy)
+    def loss(self, state, action, advantage, value_target, *args, **kwargs):
+        pred, entropy_loss, value = self.model.step(state, action)
 
-        rl_loss = reinforce_loss(pred, advantage)
+        rl_loss = -(pred * advantage).mean()
         value_loss = ((value_target - value) ** 2).mean()
 
-        loss = rl_loss + value_loss * self.value_coef *  + entropy_loss * self.entropy_coef
+        loss = rl_loss + value_loss * self.value_coef + entropy_loss * self.entropy_coef
 
         return loss, {
             "loss": loss.item(),
@@ -46,16 +78,3 @@ class ValueReinforceTrainer(BaseTrainer):
             "value_loss": value_loss.item(),
             "entropy_loss": entropy_loss.item(),
         }
-
-    def step(self, state, action, reward, advantage, value_target):
-        loss, metrics = self.loss(state, action, reward, advantage, value_target)
-
-        self.optimizer.zero_grad()
-
-        loss.backward()
-        clipped_value = torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip_value)
-        metrics["grad_norm"] = clipped_value.item()
-
-        self.optimizer.step()
-
-        return metrics

model/base.py
@@ -2,15 +2,21 @@ import torch
 from torch import nn
 import torch.distributions as dist
 import torch.nn.functional as F
+from common.utils import build_mlp
 
 
-class ModelBase(nn.Module):
+class AutoModel(nn.Module):
     def __init__(
         self,
         in_dim, 
         hidden_dim, 
         out_dim,
         device,
+        discrete=True,
+        body_depth=1,
+        value_depth=1,
+        head_depth=1,
+        clip_value=5,
         *args,
         **kwargs
     ):
@@ -19,31 +25,90 @@ class ModelBase(nn.Module):
         self.hidden_dim = hidden_dim
         self.out_dim = out_dim
         self.device = device
+        self.body_depth = body_depth
+        self.value_depth = value_depth
+        self.head_depth = head_depth
+        self.discrete = discrete
+        self.clip_value = clip_value
+
+        if self.body_depth > 0:
+            self.body = build_mlp(
+                input_dim=self.in_dim, 
+                hidden_dim=self.hidden_dim, 
+                depth=self.body_depth
+            )
+        else:
+            self.body = nn.Sequential()
+
+        assert self.head_depth > 0
+        self.head = build_mlp(
+            input_dim=self.hidden_dim if self.body_depth > 0 else self.in_dim,
+            hidden_dim=self.hidden_dim, 
+            output_dim=self.out_dim * (int(not self.discrete) + 1),
+            depth=self.head_depth
+        )
+
+        if self.value_depth > 0:
+            self.value = build_mlp(
+            input_dim=self.hidden_dim if self.body_depth > 0 else self.in_dim,
+            hidden_dim=self.hidden_dim,
+            output_dim=1,
+            depth=self.value_depth
+        )
+        else:
+            self.value = lambda x: 0
+        
+        print("Body:", self.body)
+        print("RL head:", self.head)
+        print("Value head:", self.value)
+
+    def clip(self):
+        metrics = {}
+        if self.body_depth > 0:
+            clipped_value = torch.nn.utils.clip_grad_norm_(self.parameters(), self.clip_value)
+            metrics["grad_norm"] = clipped_value.item()
+        else:
+            clipped_value = torch.nn.utils.clip_grad_norm_(self.value.parameters(), self.clip_value)
+            metrics["value_head_grad_norm"] = clipped_value.item()
+            clipped_value = torch.nn.utils.clip_grad_norm_(self.head.parameters(), self.clip_value)
+            metrics["rl_head_grad_norm"] = clipped_value.item()
+        return metrics
 
     def forward(self, x):
-        raise NotImplementedError
+        logits = self.body(x)
+        return self.head(logits), self.value(logits)
 
     def sample(self, state: torch.Tensor):
         if not isinstance(state, torch.Tensor):
             state = torch.tensor(state, device=self.device, dtype=torch.float32).unsqueeze(0)
-        assert len(state.shape) == 2 and state.shape[0] == 1
+        assert len(state.shape) == 2 and state.shape[0] == 1 # batch_size == 1
 
         pred, value = self.__call__(state)
         pred = pred.squeeze()
         value = value.squeeze()
-        categorical = dist.Categorical(logits=pred)
-        return categorical.sample().item(), value.item()
+        if self.discrete:
+            distribution = dist.Categorical(logits=pred)
+            result = distribution.sample().item()
+        else:
+            pred = pred.reshape(-1, 2)
+            result = []
+            for i in range(self.out_dim):
+                distribution = dist.Normal(loc=pred[i, 0], scale=pred[i, 1])
+                result.append(distribution.sample().item())
+        return result, value.item()
 
-    def step(self, state: torch.Tensor, action: torch.Tensor, entropy=False):
+    def step(self, state: torch.Tensor, action: torch.Tensor):
         assert len(action.shape) == 1
 
         pred, value = self.__call__(state)
+        pred = pred.log_softmax(dim=-1)
         value = value.squeeze()
-        pred_action_logit = pred[torch.arange(pred.size(0)), action]
+        if self.discrete:
+            pred_action_logit = pred[torch.arange(pred.size(0)), action]
 
-        probs = F.softmax(pred, dim=-1)
-        entropy_loss = (probs * F.log_softmax(pred, dim=-1)).sum(dim=-1).mean()
-        if not entropy:
-            entropy_loss = entropy_loss.detach()
+            probs = F.softmax(pred, dim=-1)
+            entropy_loss = (probs * F.log_softmax(pred, dim=-1)).sum(dim=-1).mean()
 
-        return pred_action_logit, entropy_loss, value
+            return pred_action_logit, entropy_loss, value
+        else:
+            entropies = []